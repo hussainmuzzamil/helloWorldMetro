8778c4427ba9388176eed93f36301c92ff65002d
hussainmuzzamil <cc.muzz@gmail.com>
2012-12-08 03:14:58 +0500

    #2418 basic server in node copied from nodejs.org

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//basic node server form their website added here just for testing
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
